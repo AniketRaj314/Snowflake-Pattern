20e7387477bffc004901538bab0772f509b580b6
Aniket Raj <aniketronaldo10@gmail.com>
2020-04-28 11:56:43 +0530

    First commit

---
+++ game files/sketch.js
@@ -0,0 +1,51 @@
+let width, height, symmetry, xOffset;
+function setup() {
+    width = 500;
+    height = 500;
+    symmetry = 6;
+    xOffset = 0;
+    createCanvas(width, height);
+    background(0);
+    clearButton = createButton('Clear');
+    clearButton.mousePressed(clearCanvas);
+
+    saveButton = createButton('Save');
+    saveButton.mousePressed(saveSnowflake);
+    angleMode(DEGREES);
+}
+
+function clearCanvas() {
+    background(0);
+}
+
+function saveSnowflake() {
+    save('snowflake.png');
+}
+
+function draw() {
+    translate(width / 2, height / 2);
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+        let mx = mouseX - width / 2;
+        let my = mouseY - height / 2;
+        let pmx = pmouseX - width / 2;
+        let pmy = pmouseY - height / 2;
+
+        if (mouseIsPressed) {
+            let hueAmount = map(sin(xOffset), -1, 1, 200, 255);
+            xOffset++;
+            stroke(hueAmount, 100);
+            let angle = 360 / symmetry;
+            for (let i = 0; i < symmetry; i++) {
+                rotate(angle);
+                strokeWeight(4);
+                line(mx, my, pmx, pmy);
+                push();
+                scale(1, -1);
+                line(mx, my, pmx, pmy);
+                pop();
+            }
+        }
+    }
+
+
+}
